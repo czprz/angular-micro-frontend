dbdf60d5c3e77aa2d8da240dd9b2d1233beb926e
czprz <57328905+czprz@users.noreply.github.com>
2022-10-18 21:23:44 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,21 +1,27 @@
 cd angular-libs
+npm install
 ng build --configuration production
 
-Copy-Item -Path dist/broker -Destination ../angular-menu/node_modules/@czprz/broker
-Copy-Item -Path dist/broker -Destination ../angular-shell/node_modules/@czprz/broker
+Copy-Item -Path ../angular-libs/dist/broker -Destination node_modules/@czprz/broker
 
 cd ..
 cd angular-menu
+npm install
 ng build --configuration production
 
 cd ..
 cd angular-overview
+npm install
 ng build --configuration production
 
 cd ..
 cd angular-toolbar
+npm install
+Copy-Item -Path ../angular-libs/dist/broker -Destination node_modules/@czprz/broker -Force
 ng build --configuration production
 
 cd ..
 cd angular-shell
+npm install
+Copy-Item -Path ../angular-libs/dist/broker -Destination node_modules/@czprz/broker -Force
 ng build --configuration production
