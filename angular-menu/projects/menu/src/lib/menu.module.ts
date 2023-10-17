3fb14144f889d320739e856893e1cb0847ec9fde
Casper Overholm Elkrog <support.open-source@elkrog.me>
2023-10-17 20:37:00 +0200

    Updated angular-libs to v15

--- angular-menu/projects/menu/src/lib/menu.module.ts
+++ angular-menu/projects/menu/src/lib/menu.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from "@angular/core";
-import { MenuComponent } from "./menu.component";
-import {RouterLink, RouterLinkWithHref} from "@angular/router";
-import { ButtonModule } from "primeng/button";
+import {NgModule} from "@angular/core";
+import {MenuComponent} from "./menu.component";
+import {RouterLink} from "@angular/router";
+import {ButtonModule} from "primeng/button";
 import {InputTextModule} from "primeng/inputtext";
 
 @NgModule({
-  declarations: [MenuComponent],
-    imports: [RouterLinkWithHref, ButtonModule, InputTextModule, RouterLink],
-  exports: [MenuComponent],
+    declarations: [MenuComponent],
+    imports: [ButtonModule, InputTextModule, RouterLink],
+    exports: [MenuComponent],
 })
-export class MenuModule {}
+export class MenuModule {
+}
